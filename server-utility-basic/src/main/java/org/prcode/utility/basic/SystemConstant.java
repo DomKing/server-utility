bc4e94d699b837aa704fe8ddda73ff1eb5317ba6
kangduo <beixianchen@gmail.com>
2017-04-16 23:19:38 +0800

    引入springSecurity及权限校验

---
+++ server-utility-basic/src/main/java/org/prcode/utility/basic/SystemConstant.java
@@ -0,0 +1,11 @@
+package org.prcode.utility.basic;
+
+/**
+ * @ClassName: SystemConstant
+ * @Date: 2017-4-16 20:04
+ * @Auther: kangduo
+ * @Description: (系统集合类)
+ */
+public class SystemConstant {
+    public static final String OOS_SYSTEM = "OOS";
+}
